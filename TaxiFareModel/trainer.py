e7b17f465f329d2efde368dcb958dcf8e5bf795d
Alban Cavey <albancavey@gmail.com>
2022-02-22 17:33:48 +0100

    Updated trainer class and other

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,4 +1,5 @@
 # imports
+from re import L
 from tkinter.tix import COLUMN
 from TaxiFareModel.data import get_data, clean_data
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
@@ -9,14 +10,23 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+import joblib
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
 
 class Trainer():
+
     def __init__(self, X, y, **kwargs):
         """
             X: pandas DataFrame
             y: pandas Series
         """
+        self.MLFLOW_URI = "https://mlflow.lewagon.co/"
         self.pipeline = None
         self.y = y
         self.X = X
@@ -26,6 +36,7 @@ class Trainer():
         self.y_train = ()
         self.y_test = ()
         self.hold_out_verif = 0
+        self.experiment_name = "[FR] [Nantes] [AlbanKv] TaxiFareModel v1.0"
 
     def hold_out(self, X, y):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.test_size)
@@ -48,7 +59,7 @@ class Trainer():
         ], remainder="drop")
         self.pipeline = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())
+            ('model', LinearRegression()) #'LinearRegression', LinearRegression()
         ])
         return self.pipeline
 
@@ -58,7 +69,8 @@ class Trainer():
             self.pipeline = self.set_pipeline().fit(self.X, self.y)
         else:
             self.pipeline = self.set_pipeline().fit(self.X_train, self.y_train)
-
+        experiment_id = trainer.mlflow_experiment_id
+        print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
 
     def evaluate(self, X_test, y_test):
         '''returns the value of the RMSE'''
@@ -66,8 +78,39 @@ class Trainer():
         rmse = compute_rmse(y_pred, y_test)
         return rmse
 
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, 'model.joblib')
+        self.mlflow_log_param('metric', LinearRegression())
+        self.mlflow_log_metric('rmse', rmse)
+        pass
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
 if __name__ == "__main__":
     # get data & clean data
+    # for model in [LinearRegression(), RandomForestRegressor()]:
     df = get_data()
     df = clean_data(df)
     # set X and y
@@ -80,4 +123,6 @@ if __name__ == "__main__":
     trainer.run()
     # evaluate
     res = trainer.evaluate(X_test, y_test)
+    # save as .joblib
+    trainer.save_model()
     print(res)
